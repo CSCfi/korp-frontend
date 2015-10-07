CSCfi/korp-frontend

commit 6ae709589ae193c230e3aeb7733ab43103d09972

Sidebar: Handle null attribute values
app/scripts/widgets.coffee:
  Display the [empty] placeholder for null attribute values in the sidebar,
    instead of causing an error. If the empty string has a translation, do
    not show the [empty] placeholder. (Sidebar.renderItem)

app/scripts/bin/widgets.js
  Compiled file.

app/scripts/bin/widgets.js
@@ -1,4 +1,4 @@
-// Generated by CoffeeScript 1.9.0
+// Generated by CoffeeScript 1.9.3
 (function() {
   var Sidebar;
 
@@ -8,10 +8,10 @@
     },
     _init: function() {},
     updateContent: function(sentenceData, wordData, corpus, tokens) {
-      var corpusObj, formattedCorpusInfo, token_data, _ref;
+      var corpusObj, formattedCorpusInfo, ref, token_data;
       this.element.html('<div id="selected_sentence" /><div id="selected_word" /><div id="selected_links" />');
       corpusObj = settings.corpora[corpus];
-      formattedCorpusInfo = (typeof settings !== "undefined" && settings !== null ? settings.corpusExtraInfo : void 0) ? util.formatCorpusExtraInfo(corpusObj, (_ref = settings.corpusExtraInfo) != null ? _ref.sidebar : void 0) : "";
+      formattedCorpusInfo = (typeof settings !== "undefined" && settings !== null ? settings.corpusExtraInfo : void 0) ? util.formatCorpusExtraInfo(corpusObj, (ref = settings.corpusExtraInfo) != null ? ref.sidebar : void 0) : "";
       if (formattedCorpusInfo) {
         formattedCorpusInfo = "<br/>" + formattedCorpusInfo;
       }
@@ -49,8 +49,8 @@
           wnd = this.contentWindow;
           tokens = tokens;
           return wnd.draw_deptree.call(wnd, tokens, function(msg) {
-            var type, val, _ref;
-            _ref = _.head(_.pairs(msg)), type = _ref[0], val = _ref[1];
+            var ref, type, val;
+            ref = _.head(_.pairs(msg)), type = ref[0], val = ref[1];
             return info.empty().append($("<span>").localeKey(type), $("<span>: </span>"), $("<span>").localeKey(type + "_" + val));
           });
         });
@@ -64,43 +64,43 @@
       var items, key, order, pairs, synthetic, value;
       pairs = _.pairs(wordData);
       order = this.options.displayOrder;
-      pairs.sort(function(_arg, _arg1) {
+      pairs.sort(function(arg, arg1) {
         var a, b;
-        a = _arg[0];
-        b = _arg1[0];
+        a = arg[0];
+        b = arg1[0];
         return $.inArray(b, order) - $.inArray(a, order);
       });
       items = (function() {
-        var _i, _len, _ref, _results;
-        _results = [];
-        for (_i = 0, _len = pairs.length; _i < _len; _i++) {
-          _ref = pairs[_i], key = _ref[0], value = _ref[1];
+        var j, len, ref, results;
+        results = [];
+        for (j = 0, len = pairs.length; j < len; j++) {
+          ref = pairs[j], key = ref[0], value = ref[1];
           if (corpus_attrs[key]) {
-            _results.push(this.renderItem(key, value, corpus_attrs[key], token_data));
+            results.push(this.renderItem(key, value, corpus_attrs[key], token_data));
           }
         }
-        return _results;
+        return results;
       }).call(this);
       if (synthetic_attr_names.length) {
         synthetic = (function() {
-          var _i, _len, _results;
-          _results = [];
-          for (_i = 0, _len = synthetic_attr_names.length; _i < _len; _i++) {
-            key = synthetic_attr_names[_i];
-            _results.push(this.renderItem(key, null, corpus_attrs[key], token_data));
+          var j, len, results;
+          results = [];
+          for (j = 0, len = synthetic_attr_names.length; j < len; j++) {
+            key = synthetic_attr_names[j];
+            results.push(this.renderItem(key, null, corpus_attrs[key], token_data));
           }
-          return _results;
+          return results;
         }).call(this);
         items = items.concat(synthetic);
       }
       return $(items);
     },
     renderItem: function(key, value, attrs, token_data) {
-      var address, getStringVal, inner, itr, li, link_text, lis, output, pattern, prefix, str_value, taginfo_url, target, ul, url, val, valueArray, x, _ref, _ref1, _ref2;
+      var address, getStringVal, inner, itr, li, link_text, lis, output, pattern, prefix, ref, ref1, ref2, ref3, str_value, taginfo_url, target, ul, url, val, valueArray, x;
       if (attrs.displayType === "hidden" || attrs.displayType === "date_interval") {
         return "";
       }
-      if (attrs.type === "url" && (attrs != null ? (_ref = attrs.url_opts) != null ? _ref.hide_url : void 0 : void 0)) {
+      if (attrs.type === "url" && (attrs != null ? (ref = attrs.url_opts) != null ? ref.hide_url : void 0 : void 0)) {
         if (value === "") {
           return "";
         }
@@ -109,8 +109,11 @@
         output = $("<p><span rel='localize[" + attrs.label + "]'>" + key + "</span>: </p>");
       }
       output.data("attrs", attrs);
-      if (value === "|" || value === "") {
-        output.append("<i rel='localize[empty]' style='color : grey'>${util.getLocaleString('empty')}</i>");
+      if (value == null) {
+        value = "";
+      }
+      if ((value === "|" || value === "") && !((attrs.translationKey != null) && (((ref1 = attrs.dataset) != null ? ref1[value] : void 0) != null))) {
+        return output.append("<i rel='localize[empty]' style='color : grey'>${util.getLocaleString('empty')}</i>");
       }
       if (attrs.type === "set") {
         pattern = attrs.pattern || '<span data-key="<% key %>"><%= val %></span>';
@@ -133,10 +136,10 @@
         }
         itr = _.isArray(valueArray) ? valueArray : _.values(valueArray);
         lis = (function() {
-          var _i, _len, _results;
-          _results = [];
-          for (_i = 0, _len = itr.length; _i < _len; _i++) {
-            x = itr[_i];
+          var j, len, results;
+          results = [];
+          for (j = 0, len = itr.length; j < len; j++) {
+            x = itr[j];
             if (!x.length) {
               continue;
             }
@@ -167,9 +170,9 @@
                 });
               });
             }
-            _results.push(li);
+            results.push(li);
           }
-          return _results;
+          return results;
         })();
         ul.append(lis);
         output.append(ul);
@@ -178,8 +181,8 @@
       str_value = attrs.stringify_synthetic ? attrs.stringify_synthetic(token_data) : (attrs.stringify || _.identity)(value);
       if (attrs.type === "url") {
         url = (attrs.url_prefix || "") + str_value;
-        target = (attrs != null ? (_ref1 = attrs.url_opts) != null ? _ref1.new_window : void 0 : void 0) ? " target='_blank'" : "";
-        link_text = (attrs != null ? (_ref2 = attrs.url_opts) != null ? _ref2.hide_url : void 0 : void 0) ? "<span rel='localize[" + attrs.label + "]'>" + key + "</span>" : decodeURI(str_value);
+        target = (attrs != null ? (ref2 = attrs.url_opts) != null ? ref2.new_window : void 0 : void 0) ? " target='_blank'" : "";
+        link_text = (attrs != null ? (ref3 = attrs.url_opts) != null ? ref3.hide_url : void 0 : void 0) ? "<span rel='localize[" + attrs.label + "]'>" + key + "</span>" : decodeURI(str_value);
         return output.append("<a href='" + url + "' class='exturl sidebar_url'" + target + ">" + link_text + "</a>");
       } else if (key === "msd" && attrs.taginfo_url !== "") {
         taginfo_url = attrs.taginfo_url || "markup/msdtags.html";
@@ -202,8 +205,8 @@
       var totalWidth;
       totalWidth = this.element.width();
       return this.element.find(".sidebar_url").css("white-space", "nowrap").each(function() {
-        var a, domain, midsection, oldtext, _results;
-        _results = [];
+        var a, domain, midsection, oldtext, results;
+        results = [];
         while ($(this).width() > totalWidth) {
           oldtext = $(this).text();
           a = $.trim(oldtext, "/").replace("...", "").split("/");
@@ -214,10 +217,10 @@
           if (midsection === "...") {
             break;
           } else {
-            _results.push(void 0);
+            results.push(void 0);
           }
         }
-        return _results;
+        return results;
       });
     },
     _parseLemma: function(attr, tmplVal) {

app/scripts/widgets.coffee
@@ -101,8 +101,15 @@ Sidebar =
         else
             output = $("<p><span rel='localize[#{attrs.label}]'>#{key}</span>: </p>")
         output.data("attrs", attrs)
-        if value == "|" or value == ""
-            output.append "<i rel='localize[empty]' style='color : grey'>${util.getLocaleString('empty')}</i>"
+        # Convert an undefined value to the empty string (Jyrki Niemi
+        # 2015-08-26)
+        value ?= ""
+        if (value == "|" or value == "") and
+                not (attrs.translationKey? and attrs.dataset?[value]?)
+            # The original version only appended to the output here
+            # but did not return yet. Would we need further processing
+            # for empty values in some cases? (Jyrki Niemi 2015-08-26)
+            return output.append "<i rel='localize[empty]' style='color : grey'>${util.getLocaleString('empty')}</i>"
 
 
         if attrs.type == "set"

commit 8a5a1005f1404edd9b24579bb8ed42a71ab228b6

Sidebar: Fixed synthetic attributes
Fixed synthetic attributes to be shown correctly in the sidebar instead of
the "[empty]" placeholder (a bug probably introduced in commit 6ae7095).

app/scripts/widgets.coffee:
  Sidebar.renderItem(): Do not return the [empty] placeholder for a null
    value if the attribute is synthetic (has the property
    stringify_synthetic).

app/scripts/bin/widgets.js:
  Compiled file.

app/scripts/bin/widgets.js
@@ -112,7 +112,7 @@
       if (value == null) {
         value = "";
       }
-      if ((value === "|" || value === "") && !((attrs.translationKey != null) && (((ref1 = attrs.dataset) != null ? ref1[value] : void 0) != null))) {
+      if ((value === "|" || value === "") && !((attrs.translationKey != null) && (((ref1 = attrs.dataset) != null ? ref1[value] : void 0) != null)) && (attrs.stringify_synthetic == null)) {
         return output.append("<i rel='localize[empty]' style='color : grey'>${util.getLocaleString('empty')}</i>");
       }
       if (attrs.type === "set") {

app/scripts/widgets.coffee
@@ -114,7 +114,8 @@ Sidebar =
         # 2015-08-26)
         value ?= ""
         if (value == "|" or value == "") and
-                not (attrs.translationKey? and attrs.dataset?[value]?)
+                not (attrs.translationKey? and attrs.dataset?[value]?) and
+                not attrs.stringify_synthetic?
             # The original version only appended to the output here
             # but did not return yet. Would we need further processing
             # for empty values in some cases? (Jyrki Niemi 2015-08-26)