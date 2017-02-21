CSCfi/korp-frontend

commit 7ed04d45a69a49659678b95f172aa35b3a62b186

Compare: Show values containing / (more) correctly
Compare searches result view: Show the attribute values containing a slash
correctly in more cases. Split the value to attributes at slashes only at
most the number of reduce attributes less one times. With multiple reduce
attributes, this still does not guarantee correct results for attribute
values containing a slash.

To implement the fix, extracted and extended the code for splitting a query
comparison value key from compareCtrl (result_controllers.coffee) to the
function util.splitCompareKey (util.coffee).

app/scripts/bin/result_controllers.js
@@ -357,18 +357,13 @@
       s.cmp1 = cmp1;
       s.cmp2 = cmp2;
       cmps = [cmp1, cmp2];
+      s.reduceIsStructAttr = _.map(reduce, function(attr) {
+        return attributes[attr].isStructAttr;
+      });
       return s.rowClick = function(row, cmp_index) {
         var cmp, cqp, cqps, k, opts, ref, results, splitTokens, tokenLength, tokens;
         cmp = cmps[cmp_index];
-        splitTokens = _.map(row.elems, function(elem, elemIdx) {
-          return _.map(elem.split("/"), function(tokens) {
-            if (attributes[reduce[elemIdx]].isStructAttr) {
-              return [tokens];
-            } else {
-              return tokens.split(" ");
-            }
-          });
-        });
+        splitTokens = util.splitCompareKey(row.elems, reduce, s.reduceIsStructAttr);
         tokenLength = splitTokens[0][0].length;
         tokens = _.map((function() {
           results = [];

app/scripts/bin/services.js
@@ -109,7 +109,7 @@
   korpApp.factory('backend', function($http, $q, utils, lexicons) {
     return {
       requestCompare: function(cmpObj1, cmpObj2, reduce) {
-        var conf, corpora1, corpora2, corpusListing, def, filterFun, params, split, xhr;
+        var attributes, conf, corpora1, corpora2, corpusListing, def, filterFun, params, reduceIsStructAttr, split, xhr;
         reduce = _.map(reduce, function(item) {
           return item.replace(/^_\./, "");
         });
@@ -123,6 +123,10 @@
           var ref;
           return ((ref = settings.corpusListing.getCurrentAttributes()[r]) != null ? ref.type : void 0) === "set";
         }).join(',');
+        attributes = _.extend({}, corpusListing.getCurrentAttributes(), corpusListing.getStructAttrs());
+        reduceIsStructAttr = _.map(reduce, function(attr) {
+          return attributes[attr].isStructAttr;
+        });
         def = $q.defer();
         params = {
           command: "loglike",
@@ -171,9 +175,7 @@
             });
             res = _.map(groups, function(value, key) {
               var abs, cqp, elems, loglike, tokenLists;
-              tokenLists = _.map(key.split("/"), function(tokens) {
-                return tokens.split(" ");
-              });
+              tokenLists = util.splitCompareKey([key], reduce, reduceIsStructAttr);
               loglike = 0;
               abs = 0;
               cqp = [];

app/scripts/result_controllers.coffee
@@ -304,24 +304,16 @@ korpApp.controller "compareCtrl", ($scope, $rootScope) ->
 
         cmps = [cmp1, cmp2]
 
+        s.reduceIsStructAttr =
+            _.map reduce, (attr) -> attributes[attr].isStructAttr
+
         # c.log 'compare: tables', tables, 'max', max, 'reduce', reduce
 
         s.rowClick = (row, cmp_index) ->
             cmp = cmps[cmp_index]
 
-            splitTokens = _.map row.elems, (elem, elemIdx) ->
-                _.map (elem.split "/"), (tokens) ->
-                    # If the attribute is a structural attribute, its
-                    # value should not be split at spaces, since the
-                    # same structural attribute value holds for all
-                    # the tokens of the sentence (at least usually).
-                    # This does not cover cases in which a positional
-                    # (token) attribute may contain spaces.
-                    if attributes[reduce[elemIdx]].isStructAttr
-                        [tokens]
-                    else
-                        tokens.split " "
-
+            splitTokens = util.splitCompareKey row.elems, reduce,
+                                               s.reduceIsStructAttr
             # c.log 'compare rowClick:', 'row', row, 'cmp', cmp, 'splitTokens', splitTokens
 
             # number of tokens in search

app/scripts/services.coffee
@@ -91,6 +91,11 @@ korpApp.factory 'backend', ($http, $q, utils, lexicons) ->
         split = _.filter(reduce, (r) -> 
             settings.corpusListing.getCurrentAttributes()[r]?.type == "set").join(',')
 
+        attributes = _.extend {}, corpusListing.getCurrentAttributes(),
+                              corpusListing.getStructAttrs()
+        reduceIsStructAttr =
+            _.map reduce, (attr) -> attributes[attr].isStructAttr
+
         def = $q.defer()
         params =
             command : "loglike"
@@ -141,8 +146,11 @@ korpApp.factory 'backend', ($http, $q, utils, lexicons) ->
                     obj.value.replace(/:\d+/g, "")
 
                 res = _.map groups, (value, key) ->
-                    tokenLists = _.map key.split("/"), (tokens) ->
-                        return tokens.split(" ")
+                    # tokenLists = _.map key.split("/"), (tokens) ->
+                    #     return tokens.split(" ")
+                    tokenLists = util.splitCompareKey [key], reduce,
+                                                      reduceIsStructAttr
+                    # c.log "tokenLists", tokenLists
 
                     loglike = 0
                     abs = 0

app/scripts/bin/util.js
@@ -1774,6 +1774,31 @@
     }
   };
 
+  util.splitCompareKey = function(key, reduce_attrs, attr_is_struct_attr) {
+    var reduce_len;
+    c.log("splitCompareKey", key, reduce_attrs, attr_is_struct_attr);
+    reduce_len = reduce_attrs.length;
+    return _.map(key, function(elem, elem_idx) {
+      var ref, split_attrs;
+      if (reduce_len > 1) {
+        split_attrs = elem.split("/");
+        if (split_attrs.length > reduce_len) {
+          [].splice.apply(split_attrs, [reduce_len, 9e9].concat(ref = split_attrs.slice(reduce_len).join("/"))), ref;
+        }
+      } else {
+        split_attrs = [elem];
+      }
+      c.log("split_attrs", split_attrs, reduce_attrs, reduce_len);
+      return _.map(split_attrs, function(tokens) {
+        if (attr_is_struct_attr[elem_idx]) {
+          return [tokens];
+        } else {
+          return tokens.split(" ");
+        }
+      });
+    });
+  };
+
 }).call(this);
 
 //# sourceMappingURL=util.js.map

app/scripts/util.coffee
@@ -1781,3 +1781,47 @@ util.setMode = (mode, override_explicit = false) ->
         if override_explicit or not params.mode
             params.mode = mode
             window.location.search = "?" + $.param(params)
+
+
+# Split a query comparison value key returned by the "loglike" command
+# of the backend. The parameter "key" is an array of keys,
+# "reduce_attrs" is the array of reduce attribute names, and
+# "attr_is_struct_attr" is a array of booleans indicating whether the
+# corresponding reduce attribute is a structural attribute. ("key" is
+# an array as in compareCtrl, but is that really needed?)
+#
+# Split the key to attribute values on "/" at most the number of
+# reduce attributes less one times. Do not split values of structural
+# attributes at spaces, since the same structural attribute value
+# holds for all the tokens of the sentence (at least usually). This
+# does not cover cases in which a positional (token) attribute may
+# contain spaces, nor multi-token searches for strucutral attributes.
+#
+# FIXME: That still does not guarantee correct results for attribute
+# values containing "/" with multiple reduce attributes, but at least
+# it works in more cases. To fix more properly, use separators that do
+# not occur in the attribute values, such as control characters. To
+# retain backward compatibility of korp.cgi, the separators could be
+# specified as parameters to korp.cgi.
+#
+# This function replaces the original inline code that does not take
+# into account the number of attributes nor structural attributes (in
+# compareCtrl (result_controllers.coffee) and backend/requestCompare
+# (services.coffee)). (Jyrki Niemi 2016-12-13)
+
+util.splitCompareKey = (key, reduce_attrs, attr_is_struct_attr) ->
+    c.log "splitCompareKey", key, reduce_attrs, attr_is_struct_attr
+    reduce_len = reduce_attrs.length
+    _.map key, (elem, elem_idx) ->
+        if reduce_len > 1
+            split_attrs = elem.split "/"
+            if split_attrs.length > reduce_len
+                split_attrs[reduce_len..] = split_attrs[reduce_len..].join("/")
+        else
+            split_attrs = [elem]
+        c.log "split_attrs", split_attrs, reduce_attrs, reduce_len
+        _.map split_attrs, (tokens) ->
+            if attr_is_struct_attr[elem_idx]
+                [tokens]
+            else
+                tokens.split " "

commit ba383bc16fba94c359060446d33fb728c3285ef1

Compare: Fixed to show values if compiled on word
Compare searches result view: Show attribute values when the comparison is
compiled on word, which usually has no entry in the attributes property of
a corpus configuration. Before this fix, the values for "word" in the
compare result were shown as empty because of a JavaScript error. (This bug
was introduced in commit 7ed04d45 "Compare: Show values containing / (more)
correctly".)

app/scripts/bin/result_controllers.js
@@ -357,7 +357,8 @@
       s.cmp2 = cmp2;
       cmps = [cmp1, cmp2];
       s.reduceIsStructAttr = _.map(reduce, function(attr) {
-        return attributes[attr].isStructAttr;
+        var ref;
+        return (ref = attributes[attr]) != null ? ref.isStructAttr : void 0;
       });
       return s.rowClick = function(row, cmp_index) {
         var cmp, cqp, cqps, k, opts, ref, results, splitTokens, tokenLength, tokens;

app/scripts/bin/services.js
@@ -124,7 +124,8 @@
         }).join(',');
         attributes = _.extend({}, corpusListing.getCurrentAttributes(), corpusListing.getStructAttrs());
         reduceIsStructAttr = _.map(reduce, function(attr) {
-          return attributes[attr].isStructAttr;
+          var ref;
+          return (ref = attributes[attr]) != null ? ref.isStructAttr : void 0;
         });
         def = $q.defer();
         params = {

app/scripts/result_controllers.coffee
@@ -305,7 +305,7 @@ korpApp.controller "compareCtrl", ($scope, $rootScope) ->
         cmps = [cmp1, cmp2]
 
         s.reduceIsStructAttr =
-            _.map reduce, (attr) -> attributes[attr].isStructAttr
+            _.map reduce, (attr) -> attributes[attr]?.isStructAttr
 
         # c.log 'compare: tables', tables, 'max', max, 'reduce', reduce
 

app/scripts/services.coffee
@@ -94,7 +94,7 @@ korpApp.factory 'backend', ($http, $q, utils, lexicons) ->
         attributes = _.extend {}, corpusListing.getCurrentAttributes(),
                               corpusListing.getStructAttrs()
         reduceIsStructAttr =
-            _.map reduce, (attr) -> attributes[attr].isStructAttr
+            _.map reduce, (attr) -> attributes[attr]?.isStructAttr
 
         def = $q.defer()
         params =