aa4eee3cf9bb2bd7050115cde65af78e9e899057
Aleksi Sahala <aleksi.sahala@helsinki.fi>
2019-06-04 15:41:41 +0300

    fixed error in oracc defs

--- app/modes/common.js
+++ app/modes/common.js
@@ -1966,9 +1966,9 @@ attrlist.oracc = {
     standard : {
         label : "oracc_standardized"
     },
-    /*
+
     lang : {
-        label : "oracc_lang"
+        label : "oracc_lang",
         displayType : "select",
         translationKey : "oracc_lang_",
         dataset : {
@@ -2008,7 +2008,6 @@ attrlist.oracc = {
         label : "oracc_url",
         type : "url"
     }
-    */
 };
 
 
