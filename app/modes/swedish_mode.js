2282484076a94e1b7a62fd47af894f46b225302c
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2019-12-05 11:55:14 +0200

    Config Ylenews-sv: Moved after Semfinlex
    Config Swedish: Ylenews-sv:
    - Moved the "Yle svenska webbartiklar" corpus folder after Semfinlex, so
      that the former probably is shown after the latter in the corpus
      selector (more alphabetical).

--- app/modes/swedish_mode.js
+++ app/modes/swedish_mode.js
@@ -130,6 +130,16 @@ settings.corporafolders.fstc = {
     },
 };
 
+settings.corporafolders.semfinlex = {
+    title: "Semfinlex",
+    description: "Innehåller ett urval av ursprungliga författningar av Riksdagen (1920–2018), avgöranden av Högsta domstolen (1980–) och avgöranden av Högsta förvaltningsdomstolen (2001–).",
+    contents: ["semfinlex_asd_sv_2018", "semfinlex_kko_sv_2018", "semfinlex_kho_sv_2018"],
+    info: {
+	licence: settings.licenceinfo.CC_BY,
+	cite_id: "semfinlex",
+    }
+}
+
 settings.corporafolders.ylenews_sv = {
     title: "Yle svenska webbartiklar",
     description: "Yle svenska webbartiklar<br/><br/>Mappen innehåller två korpusar med samma meningar men med olika tillgänglighet och egenskaper: den ena korpusen är öppen för alla och har meningarna i en blandad ordning inom varje text och utan utökad kontextvisning, medan den andra korpusen är tillgänglig för forskare och har meningarna i den ursprungliga ordningen och stöd för utökad kontextvisning.",
@@ -170,16 +180,6 @@ settings.corporafolders.ylenews_sv.s = {
 	cite_id: "ylenews-sv-2012-2018-s-korp",
     },
 };
-
-settings.corporafolders.semfinlex = {
-    title: "Semfinlex",
-    description: "Innehåller ett urval av ursprungliga författningar av Riksdagen (1920–2018), avgöranden av Högsta domstolen (1980–) och avgöranden av Högsta förvaltningsdomstolen (2001–).",
-    contents: ["semfinlex_asd_sv_2018", "semfinlex_kko_sv_2018", "semfinlex_kho_sv_2018"],
-    info: {
-	licence: settings.licenceinfo.CC_BY,
-	cite_id: "semfinlex",
-    }
-}
 
 
 var klk_sv_parsed_years = settings.fn.make_yearlist(1771, 1948);
