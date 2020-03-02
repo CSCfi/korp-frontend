9354ee310e06e37e4796d84c6838bec1f0400a4d
eaxelson <erik.axelson@helsinki.fi>
2020-03-02 16:46:03 +0200

    Fix id.

--- app/modes/parallel_mode.js
+++ app/modes/parallel_mode.js
@@ -8538,7 +8538,7 @@ settings.corpora.pabivus_koi_1996 = {
 };
 
 settings.corpora.pabivus_fin_1938 = {
-    id: "pabivus_fin_test",
+    id: "pabivus_fin_1938",
     title: "Pabivus (fin–koi–kpv–krl–mdf–myv–olo–udm)",
     description: "Parallel Bible verses for Uralic languages<br/>The corpus contains verses from the New Testament in eight languages: Finnish (fin), Komi-Permyak (koi), Komi-Zyrian (kpv), North Karelian (krl), Moksha (mdf), Erzya (myv), Livvi-Karelian (olo) and Udmurt (udm).<br/>Some books have more than one translation available in the same language. The translation year is shown for each subcorpus.",
     lang: "fin",
