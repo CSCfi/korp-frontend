d269d82d815ce56be03b2c6a375f05e9229a44c5
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2021-03-09 13:44:46 +0200

    config_corpus_features: Convert to a plugin
    app/plugins/config_corpus_features/scripts/main.js:
    - Convert to a plugin class with a callback for hook point
      modifyCorpusConfigs and register it using plugins.register.
    - Initialize settings.corpus_features.

--- app/plugins/config_corpus_features/scripts/main.js
+++ app/plugins/config_corpus_features/scripts/main.js
@@ -1,38 +1,75 @@
 
-// Initialize properties in settings.corpora.* based on their property
-// "features" and the properties in settings.corpus_features.
-
-util.initCorpusSettingsFeatures = () => util.forAllCorpora(util.setCorpusFeatures);
-
-// Extend corpus settings based on the values specified in the
-// "features" property of a corpus configuration.
+// Plugin config_corpus_features
 //
-// The "features" property is an array, whose values should be keys
-// (property names) of settings.corpus_features. The corpus
-// configuration is extended by the corresponding property value in
-// settings.corpus_features. Extension is recursive, so that if you
-// specify, e.g. { attributes: ... }, the specified attributes are
-// added to (instead of replacing) the explicit attributes in the
-// corpus configuration.
-//
-// Would there be a case for recursive setting of features, so that if
-// a value in settings.corpus_features contains a "features" property,
-// the properties corresponding to it are also added? If so, should the
-// properties specified by the nested "features" added before or after
-// adding the explicit properties? (Jyrki Niemi 2016-10-18)
+// Callback method to initialize properties in settings.corpora.*
+// based on their property "features" and the properties in
+// settings.corpus_features.
 
-util.setCorpusFeatures = function (corpusInfo) {
-    for (let featname of (corpusInfo.features || [])) {
-        const features = (settings.corpus_features != null
-                          ? settings.corpus_features[featname]
-                          : undefined);
-        // c.log "setCorpusFeatures", featname, features, corpusInfo
-        if (! features) {
-            c.warn("Warning: settings.corpus_features[\"" + featname +
-                   "\"] not defined: referred to in settings.corpora." +
-                   corpusInfo.id);
-        } else {
-            $.extend(true, corpusInfo, features);
+
+console.log("plugin config_corpus_features")
+
+
+// Class ConfigCorpusFeatures does not have internal state, but a
+// class is defined so that the callback functions can call internal
+// methods containing the actual implementation.
+
+class ConfigCorpusFeatures {
+
+    // Callback method called at a hook point
+
+    // Call _setCorpusFeatures for all corpora in corpora
+    modifyCorpusConfigs (corpora) {
+        // c.log("ConfigCorpusFeatures.modifyCorpusConfigs", corpora)
+        for (let corpusId in corpora) {
+            this._setCorpusFeatures(corpora[corpusId])
         }
     }
+
+    // Internal methods
+
+    // Extend corpus settings based on the values specified in the
+    // "features" property of a corpus configuration.
+    //
+    // The "features" property is an array, whose values should be
+    // keys (property names) of settings.corpus_features. The corpus
+    // configuration is extended by the corresponding property value
+    // in settings.corpus_features. Extension is recursive, so that if
+    // you specify, e.g. { attributes: ... }, the specified attributes
+    // are added to (instead of replacing) the explicit attributes in
+    // the corpus configuration.
+    //
+    // Would there be a case for recursive setting of features, so
+    // that if a value in settings.corpus_features contains a
+    // "features" property, the properties corresponding to it are
+    // also added? If so, should the properties specified by the
+    // nested "features" added before or after adding the explicit
+    // properties? (Jyrki Niemi 2016-10-18)
+
+    _setCorpusFeatures (corpusObj) {
+        for (let featname of (corpusObj.features || [])) {
+            const features = (settings.corpus_features != null
+                              ? settings.corpus_features[featname]
+                              : undefined);
+            // c.log("_setCorpusFeatures", featname, features, corpusObj)
+            if (! features) {
+                c.warn("Warning: settings.corpus_features[\"" + featname +
+                       "\"] not defined: referred to in settings.corpora." +
+                       corpusObj.id);
+            } else {
+                $.extend(true, corpusObj, features);
+            }
+        }
+    }
+
 };
+
+
+// Initialize settings.corpus_features to an empty object if it is not
+// yet initialized
+if (settings.corpus_features === undefined) {
+    settings.corpus_features = {}
+}
+
+
+// Register the plugin
+plugins.register(new ConfigCorpusFeatures())
